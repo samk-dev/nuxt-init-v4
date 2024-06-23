91e1654298c65a2cb1e9ef20e90c012e787a9f8b
Malik <62953363+maliksvd@users.noreply.github.com>
2024-06-22 22:09:57 -0400

    feat: add translations page with dynamic seo content

---
+++ app/pages/translations.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ t('page-translations-title') }}</p>
+    <p>{{ t('page-translations-description') }}</p>
+    <p>{{ t('page-translations-image') }}</p>
+
+    <AppLangSwitcher />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { t } = useI18n()
+
+const dynamicTitle = computed(() => t('page-translations-title'))
+const dynamicDescription = computed(() => t('page-translations-description'))
+const dynamicImage = computed(() => t('page-translations-image'))
+
+useSeoMeta({
+  title: dynamicTitle,
+  ogTitle: dynamicTitle,
+  description: dynamicDescription,
+  ogDescription: dynamicDescription,
+  ogImage: dynamicImage,
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<style></style>
